6032faf57300d7fe04f223046adce99f6321d4b7
azihakim <azi.hakim@mhs.mdp.ac.id>
2022-10-31 23:23:11 +0700

    new

---
+++ app/src/main/java/com/si5a/pabuts_2226240149p/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.si5a.pabuts_2226240149p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
